8b7ad8d9542e55a61dc32125437f11d872f20876
lutfii <lutfiakmal72@myra.com.my>
2024-08-11 00:44:17 +0800

    Action in Livewire

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,7 +1,18 @@
 <div>
     <form action="">
         <label for="">Poll Title</label>
-        <input type="text" wire:model="title"/>
+        <input type="text" wire:model="title" />
         Current title: {{ $title }}
+        <div class="mb-4 mt-4">
+            <button class="btn" wire:click.prevent="addOption">Add Option</button>
+        </div>
+        <div class="">
+            @foreach ($options as $index => $option)
+                <div class="mb-4">
+                    {{ $index }} - {{ $option }}
+                </div>
+            @endforeach
+        </div>
+
     </form>
 </div>
