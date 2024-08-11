0081d404bc7d120372e832a93faab6648b79dc1e
lutfii <lutfiakmal72@myra.com.my>
2024-08-11 11:46:24 +0800

    finish

--- app/Http/Livewire/Polls.php
+++ app/Http/Livewire/Polls.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 
 use App\Models\Poll;
+use App\Models\Option;
 
 class Polls extends Component
 {
@@ -12,9 +13,15 @@ class Polls extends Component
     protected $listeners = [
         'pollCreated' => 'render'
     ];
+
     public function render()
     {
         $polls = Poll::with('options.votes')->latest()->get();
         return view('livewire.polls', ['polls' => $polls]);
+    }
+
+    public function vote(Option $option)
+    {
+        $option->votes()->create();
     }
 }
